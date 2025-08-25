a4ea035b0ffeb501e3f8db9ba51cf734cac4453e
DESKTOP-QU25RS7\dev <whyis2cool@gmail.com>
2025-08-25 10:32:13 -0600

    Add a Themes folder

--- Scripts/pixel.gd
+++ Scripts/pixel.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	raw_image = Image.create_empty(DEFAULT_WIDTH , DEFAULT_HEIGHT, true , Image.Format.FORMAT_RGBA8)
 	image_texture = ImageTexture.create_from_image(raw_image)
 	self.texture = image_texture
-	self.position = Vector2(0,0)
 	current_color = Color.BLACK
 	sprite_rect = get_sprite_hitbox()
 	
@@ -29,26 +28,26 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("l_click"):
 		var mouse_pos := to_local(get_global_mouse_position())
 		var is_inside_hitbox :bool = sprite_rect.has_point(mouse_pos)
-		print("is insde hitbox %s" % is_inside_hitbox)
 		if !is_inside_hitbox : return
+		#print("is insde hitbox %s" % is_inside_hitbox)
 		var inside_sprite_pos:Vector2 = mouse_pos - sprite_rect.position
+		@warning_ignore_start("narrowing_conversion")
 		raw_image.set_pixel(inside_sprite_pos.x,inside_sprite_pos.y,Color.GOLD)
+		@warning_ignore_restore("narrowing_conversion")
 		image_texture.update(raw_image)
 		self.texture = image_texture
-		pass
-	
-	if event.is_action_pressed("move_right"):
-		self.position.x += 10.0
 		pass
 	pass
 
 func _draw() -> void:
 	if !OS.is_debug_build() : return
 	draw_circle(sprite_rect.position,10.0,Color.YELLOW)
+	var rect_color := Color.YELLOW
+	rect_color.a = 0.5;
+	draw_rect(sprite_rect,rect_color)
 	pass
 
 
 func get_sprite_hitbox() -> Rect2:
 	var sprite_size := Vector2(self.texture.get_width(),self.texture.get_height())
-	var hitbox_offset :Vector2 = Vector2(sprite_size.x * 0.5,sprite_size.y * 0.5)
-	return Rect2(self.position - hitbox_offset ,sprite_size) ;
+	return Rect2(self.get_rect().position ,sprite_size) ;
