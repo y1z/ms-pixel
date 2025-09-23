6f8022b95703e82d0b145f7793113dc1f6473abc
Yhaliff <whyis2cool@gmail.com>
2025-09-23 11:07:29 -0600

    Add constants that represent input actions.

---
+++ Scripts/input_names.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name InputNames
+
+const click_l:StringName = "click_l"
+const click_r:StringName = "click_r"
+
+const move_up:StringName = "move_up"
+const move_down:StringName = "move_down"
+const move_right:StringName = "move_right"
+const move_left:StringName = "move_left"
+
+const scroll_up:StringName = "scroll_up"
+const scroll_down:StringName = "scroll_down"
