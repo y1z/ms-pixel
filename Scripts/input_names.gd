f73a9eb3bffc16caca5268181e025a229eb245e3
Yhaliff <whyis2cool@gmail.com>
2025-09-24 23:41:45 -0600

    Add new inputNames (for the new input acctions added)

--- Scripts/input_names.gd
+++ Scripts/input_names.gd
@@ -1,6 +1,7 @@
-extends Node
+extends Object
 class_name InputNames
-
+### This is done to avoid the convertion from String to StringName
+### The names of the input actions
 const click_l:StringName = "click_l"
 const click_r:StringName = "click_r"
 
@@ -11,3 +12,7 @@ const move_left:StringName = "move_left"
 
 const scroll_up:StringName = "scroll_up"
 const scroll_down:StringName = "scroll_down"
+const scroll_up_fast:StringName = "scroll_up_fast"
+const scroll_down_fast:StringName = "scroll_down_fast"
+
+const turn_on_debug: StringName = "turn_on_debug"
