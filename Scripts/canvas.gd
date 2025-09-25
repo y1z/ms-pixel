72fea0df467a553466cac7416067219de7ca0413
Yhaliff <whyis2cool@gmail.com>
2025-09-24 23:40:51 -0600

    formatting

--- Scripts/canvas.gd
+++ Scripts/canvas.gd
@@ -6,19 +6,19 @@ const DEFAULT_HEIGHT:int = 128
 #endregion
 
 @export_group("VARIABLES")
-@export var canvas_size :Vector2i = Vector2i(DEFAULT_WIDTH, DEFAULT_HEIGHT) 
-var raw_image : Image
+@export var canvas_size:Vector2i = Vector2i(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+var raw_image: Image
+
 
 func _ready() -> void:
 	inicialize_canvas()
 	#raw_image.save
-	var thing := PixelCanvas.new();
-	print("[LOG] is pixel Canvas working = %s" % thing.test_is_working())
 	pass
+
 
 func inicialize_canvas() -> void:
 	pass
 
 
-func save_canvas() ->bool :
+func save_canvas() ->bool:
 	return false
