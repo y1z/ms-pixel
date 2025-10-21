1876243bd180edef34893cc9b9f2e4ffdec30140
Yhaliff <whyis2cool@gmail.com>
2025-10-21 17:02:50 -0600

    add script for setting up proyects

---
+++ Scripts/Editor/setup_project_script.gd
@@ -0,0 +1,22 @@
+@tool
+extends EditorScript
+class_name SetupProjectScript
+
+const proj_dir = [
+	"res://",
+	"res://",
+	"res://",
+	"res://",
+	"res://",
+	"res://",
+	"res://",
+	"res://",
+]
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run() -> void:
+	pass
+
+
+func generate_directorys() -> void:
+	pass
