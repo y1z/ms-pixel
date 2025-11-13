ab2e9f5e54fdeb9cf108707e98f49392c3de0eb5
Yhaliff <whyis2cool@gmail.com>
2025-11-12 18:40:59 -0600

    Fixed drawing

--- Scripts/Pixel Canvas/gd_pixel_canvas.gd
+++ Scripts/Pixel Canvas/gd_pixel_canvas.gd
@@ -15,24 +15,25 @@ var height: int
 
 func _ready() -> void:
 	sprite = %Canvas
+	UserData.draw_modes = UserData.DrawModes.NONE
 	start();
 	pass
 
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed(InputNames.click_l):
-		var mouse_pos := to_local(get_global_mouse_position())
-		var is_inside_hitbox:bool = sprite_rect.has_point(mouse_pos)
+		UserData.print_draw_mode()
+		match UserData.draw_modes:
+			UserData.DrawModes.NONE:
+				put_pixel()
+				return
 
-		#print("is insde hitbox [%s]" % is_inside_hitbox)
-		if !is_inside_hitbox: return
-		var inside_sprite_pos:Vector2 = mouse_pos - sprite_rect.position
+			UserData.DrawModes.PUT_PIXEL:
+				put_pixel()
+				return
 
-		@warning_ignore_start("narrowing_conversion")
-		raw_image.set_pixel(inside_sprite_pos.x, inside_sprite_pos.y, UserData.current_color)
-		@warning_ignore_restore("narrowing_conversion")
-		texture.update(raw_image)
-		sprite.texture = texture
+	if Input.is_action_just_released(InputNames.click_l):
+		UserData.draw_modes = UserData.DrawModes.NONE
 
 	pass
 
@@ -146,7 +147,70 @@ func copy_existing_image(target:Image, src:Image, copy_within_limits:bool = true
 
 
 func draw_line_on_canvas(start_:Vector2i,end_:Vector2i) -> void:
+	var delta := end_ - start_
 
+	if delta.x == 0 && delta.y == 0:
+		_only_draw_1_pixel()
+		return
+
+	if delta.x == 0 && abs(delta.y) > 0:
+		var goes_up:bool = true if delta.y > 0 else false
+		draw_vertical_line_on_canvas(start_, abs(delta.y), goes_up)
+		return
+
+	if abs(delta.x) > 0 && delta.y == 0:
+		var goes_right: bool = true if delta.x > 0 else false
+		draw_horizontal_line_on_canvas(start_, abs(delta.x), goes_right)
+		return
+
+	_simple_method(start_, end_)
+	pass
+
+
+## Gupta and Sproull algorithm
+##@tutorial(link to wiki):https://en.wikipedia.org/wiki/Line_drawing_algorithm#Gupta_and_Sproull_algorithm
+func _gupta_and_sproull_algo(start_:Vector2i, end_:Vector2i) -> void:
+	var delta:Vector2i = end_ - start_;
+	var discriminator := 2 * (delta.y - delta.x)
+	var euclidean_distance := 0.0;
+	pass
+
+
+## Simple Method
+func _simple_method(start_:Vector2i, end_:Vector2i) -> void:
+	var delta := end_ - start_;
+	var slope:float = float(delta.y) / float(delta.x)
+	var difference_between_x:int = abs(delta.x)
+	var minimum_x:int = min(end_.x, start_.x)
+	var maximum_x:int = max(end_.x, start_.x)
+
+	for x in range(minimum_x, maximum_x):
+		var y_position := floori(slope * (x - start_.x) + start_.y)
+		var position_before_correction := Vector2i(x, y_position);
+		var is_inside_hitbox:bool = sprite_rect.has_point(position_before_correction)
+
+		if !is_inside_hitbox: continue
+		var corrected := Vector2(position_before_correction) - sprite_rect.position
+
+		raw_image.set_pixel(corrected.x, corrected.y, UserData.current_color)
+
+	update_texture()
+	pass
+
+
+func draw_vertical_line_on_canvas(start_:Vector2i, distance_:int, goes_up:bool) -> void:
+	var current_position := start_;
+
+	var increment:int = 1 if goes_up else -1;
+	for i in distance_:
+		var is_inside_hitbox:bool = sprite_rect.has_point(current_position)
+
+		if !is_inside_hitbox: continue
+		var final_pos := Vector2(current_position) - sprite_rect.position
+		raw_image.set_pixelv(final_pos, UserData.current_color)
+		current_position.y += increment
+
+	update_texture()
 	pass
 
 
@@ -155,8 +219,45 @@ func draw_horizontal_line_on_canvas(start_:Vector2i, distance_:int, goes_left_to
 
 	var increment:int = 1 if goes_left_to_right else -1;
 	for i in distance_:
-		raw_image.set_pixelv(current_position, UserData.current_color)
-		pass
+		var is_inside_hitbox:bool = sprite_rect.has_point(current_position)
+
+		if !is_inside_hitbox: continue
+		var final_pos := Vector2(current_position) - sprite_rect.position
+		raw_image.set_pixelv(Vector2i(final_pos), UserData.current_color)
+		current_position.x += increment
 
 	update_texture()
+	pass
+
+
+func put_pixel() -> void:
+
+	match UserData.draw_modes:
+		UserData.DrawModes.NONE:
+			UserData.start_point = get_local_mouse_position();
+			_only_draw_1_pixel()
+			UserData.draw_modes = UserData.DrawModes.PUT_PIXEL
+
+		UserData.DrawModes.PUT_PIXEL:
+			UserData.end_point = get_local_mouse_position()
+			draw_line_on_canvas(UserData.start_point, UserData.end_point)
+			UserData.start_point = UserData.end_point
+
+		_:
+			printerr("UN-HANDLED CASE %s " % UserData.keys()[UserData.draw_modes])
+
+	pass
+
+
+func _only_draw_1_pixel() -> void:
+	var mouse_pos := get_local_mouse_position()# to_local(get_global_mouse_position())
+	var is_inside_hitbox:bool = sprite_rect.has_point(mouse_pos)
+	#print("is insde hitbox [%s]" % is_inside_hitbox)
+	if !is_inside_hitbox: return
+	var inside_sprite_pos:Vector2 = mouse_pos - sprite_rect.position
+	@warning_ignore_start("narrowing_conversion")
+	raw_image.set_pixel(inside_sprite_pos.x, inside_sprite_pos.y, UserData.current_color)
+	@warning_ignore_restore("narrowing_conversion")
+	texture.update(raw_image)
+	sprite.texture = texture
 	pass
