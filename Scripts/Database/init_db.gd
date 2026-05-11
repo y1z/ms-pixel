193fa4e74e6b650e76fc5a262497053d520f0990
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-05-11 17:34:10 -0500

    Add Database to project

---
+++ Scripts/Database/init_db.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var database:SQLite
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	database = SQLite.new()
+	database.path="res://data.db"
+	database.open_db()
+	pass # Replace with function body.
